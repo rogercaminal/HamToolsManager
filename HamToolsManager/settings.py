3cc04d54ebea0ee13b3a7c8bc6d617dcf4d122ca
Roger Caminal Armadans <roger.caminal@gmail.com>
2017-10-06 16:20:05 +0200

    Adding css files

--- HamToolsManager/settings.py
+++ HamToolsManager/settings.py
@@ -119,4 +119,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.10/howto/static-files/
 
 STATIC_URL = '/static/'
-STATICFILES_DIRS = ( os.path.join(BASE_DIR, "ContestAnalyzerOnline/contestAnalyzer/data/"), )
+STATICFILES_DIRS = ( 
+        os.path.join(BASE_DIR, 'static'),
+        os.path.join(BASE_DIR, 'ContestAnalyzerOnline/static'),
+        os.path.join(BASE_DIR, "ContestAnalyzerOnline/contestAnalyzer/data/"), 
+        )
